08631c20000c4bc64ed64bec21935b57335349f4
YTCai <qazxcv367tasi@gmail.com>
2018-05-30 17:36:25 +0800

    users section working 100% exception security

--- app/Http/Controllers/AdminUserController.php
+++ app/Http/Controllers/AdminUserController.php
@@ -8,6 +8,7 @@ use App\Role;
 use App\Photo;
 use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 
 
@@ -48,7 +49,18 @@ class AdminUserController extends Controller
     {
         //
         // return $request->all();
-        $input = $request->all();
+
+        if (trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else {
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
         
 
         if ($file = $request->file('photo_id')){
@@ -63,11 +75,9 @@ class AdminUserController extends Controller
 
         }
 
-        $input['password'] = bcrypt($request->password);
-
         User::create($input);
 
-        // return redirect('/admin/users');
+        return redirect('/admin/users');
 
 
 
@@ -95,7 +105,10 @@ class AdminUserController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.users.edit');
+        $user = User::findOrFail($id);
+        $roles = Role::lists('name', 'id')->all();
+
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -105,9 +118,39 @@ class AdminUserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+        $user = User::findOrFail($id);
+
+        if (trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        }else {
+
+            $input = $request->all();
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+        if($file = $request->file('photo_id')){
+
+           $name = time() . $file->getClientOriginalName();
+
+           $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
+
+
     }
 
     /**
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+    <h1>Edit User</h1>
+    <div class="row">
+        <div class="col-sm-3">
+
+            <img src="{{ $user->photo ? $user->photo->file : 'http://placehold.it/400x400' }}" alt="" class="img-responsive img-rounded">
+
+        </div>
+
+
+        <div class="col-sm-9">
+        
+            {!! Form::model($user, ['method'=>'PATCH', 'action'=> ['AdminUserController@update', $user->id], 'files'=>true]) !!}
+            
+            <div class="form-group">
+                
+                {!! Form::label('name', 'Name:') !!}
+                
+                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form-group">
+                
+                {!! Form::label('email','Email:') !!}
+                
+                {!! Form::text('email', null, ['class'=>'form-control']) !!}
+                
+            </div>
+
+            <div class="form-group">
+                
+                {!! Form::label('role_id','Role:') !!}
+                
+                {!! Form::select('role_id', $roles, null, ['class'=>'form-control']) !!}
+                
+            </div>
+
+            <div class="form-group">
+                
+                {!! Form::label('is_active','Status:') !!}
+                
+                {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), null, ['class'=>'form-control']) !!}
+                
+            </div>
+
+            <div class="form-group">
+                
+                {!! Form::label('photo_id','Photo:') !!}
+                
+                {!! Form::file('photo_id', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+                
+            </div>    
+            
+            <div class="form-group">
+                
+                {!! Form::label('password','Password:') !!}
+                
+                {!! Form::text('password', null, ['class'=>'form-control']) !!}
+                
+            </div>
+
+            <div class="form-group">
+                
+                {!! Form::submit('Create Post', ['class'=>'btn btn-primay']) !!}
+                
+            </div>
+
+            
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+    <div class="row">
+        
+        @include('includes.form_error')
+
+    </div>
+    
+    
+
+
+    
+@endsection
